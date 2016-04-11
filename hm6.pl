811d5622489459a659f3b87a412374f4a9cde57a
Oleksandr Halahan <Oleksandr.Halahan@gmail.com>
2016-04-11 11:27:30 +0300

    improved tags hash filling; hm6 is finished

--- hm6.pl
+++ hm6.pl
@@ -15,18 +15,13 @@ sub print_result {
 }
 
 while (<$fh>) {
-    my @row;
     chomp($tag = $_);
     #disting tags (opening/closing) from plain text
     ($plain_text = $tag) =~ s/<[^>]*>//gs;
     #create hash: word => count
     $words{lc($&)}++ while ($plain_text =~ m/\b\w+\b/igs);
     #create hash: tag => count
-    my @tags_durty;
-    push(@tags_durty, $&) while ($tag =~ m/<[a-z]+>?/igs);
-    for (@tags_durty) {
-        $tags{$&}++ while ($_ =~ m/[a-z]+/igs);
-    }
+    $tags{$1}++ while ($tag =~ m/<([a-z]+)>?/igs);
 } 
 
 print_result(\%words, "words", '"', '"');
