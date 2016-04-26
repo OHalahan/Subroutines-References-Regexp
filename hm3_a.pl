a2914a11d20df6e7cac6ab4329649bc75df4a651
Oleksandr Halahan <Oleksandr.Halahan@gmail.com>
2016-04-26 07:37:22 +0300

    started changing hm3

--- hm3_a.pl
+++ hm3_a.pl
@@ -9,18 +9,20 @@ open( my $fh, "<", $dict ) or die "Cannot open a file $dict: $!\n";
 print "Enter the word for search:\nNote: the search is case insensitive\n\n";
 while (<>) {
     chomp;
-    my ($word, $found, $count) = (lc);
+    my ( $word, $found, $count ) = (lc);
     seek ($fh, 0, 0);
 
     while (<$fh>) {
         chomp;
         $count++;
-        if (lc eq $word) {
+        if ( lc eq $word ) {
             print "Found $_ at line $count\n";    
             $found++;
             last;
         }
     }
-    print "No matches found!\n" unless $found;
+    if (!$found) {
+        print "No matches found!\n";    
+    }
     print "\nTry again:\n";
 } 
