fb9a386b194a38950bacb914ddddb08b788f560f
Oleksandr Halahan <Oleksandr.Halahan@gmail.com>
2016-04-26 07:28:01 +0300

    added third hm

---
+++ hm3_a.pl
@@ -0,0 +1,26 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+my $dict = '/etc/dictionaries-common/words';
+open( my $fh, "<", $dict ) or die "Cannot open a file $dict: $!\n";
+
+print "Enter the word for search:\nNote: the search is case insensitive\n\n";
+while (<>) {
+    chomp;
+    my ($word, $found, $count) = (lc);
+    seek ($fh, 0, 0);
+
+    while (<$fh>) {
+        chomp;
+        $count++;
+        if (lc eq $word) {
+            print "Found $_ at line $count\n";    
+            $found++;
+            last;
+        }
+    }
+    print "No matches found!\n" unless $found;
+    print "\nTry again:\n";
+} 
