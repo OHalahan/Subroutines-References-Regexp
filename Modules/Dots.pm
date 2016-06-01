0dca491146b3e4779e429f5176a7533936fab8a1
Oleksandr Halahan <Oleksandr.Halahan@portaone.com>
2016-06-01 09:43:06 +0300

    added autoload changed formatting

--- Modules/Dots.pm
+++ Modules/Dots.pm
@@ -6,69 +6,50 @@ use strict;
 use warnings;
 use feature "switch";
 
-sub get_x {
-    my $self = shift;
-    return $self->{x};
-}
-
-sub get_y {
-    my $self = shift;
-    return $self->{y};
-}
-
-sub set_x {
-    my $self = shift;
-    $self->{x} = shift;
-}
-
-sub set_y {
-    my $self = shift;
-    $self->{y} = shift;
-}
-
 sub new {
     my $class = shift;
     my $self = {};
-    bless($self, $class);
+    bless( $self, $class );
     $self->init(@_);
     return $self;
 }
 
 sub init {
     my $self = shift;
-    my ($gl_x, $gl_y, $objects) = @_;
-    my ($x, $y) = (int rand $gl_x, int rand $gl_y);
+    my ( $gl_x, $gl_y, $objects ) = @_;
+    my ( $x, $y ) = ( int rand $gl_x, int rand $gl_y );
     # check that object is created not on the another object or obstacle
-    unless (check($x, $y, $objects, $gl_x, $gl_y)) {
+    unless ( check( $x, $y, $objects, $gl_x, $gl_y ) ) {
         $self->{x} = $x;
-        $self->{y} = $y;  
+        $self->{y} = $y;
     } else {
         $self->init(@_);
     }
 }
 
 sub check {
-    my ($x, $y, $objects, $global_x, $global_y) = @_;
-    return 1 unless (($x < $global_x) and ($x > 0) and ($y < $global_y) and ($y > 0));
-    for (@{$objects}) {
-        if (($x == $_->get_x) and ($y == $_->get_y)) {return 1};
+    my ( $x, $y, $objects, $global_x, $global_y ) = @_;
+    return 1 unless ( ( $x < $global_x ) and ( $x > 0 ) and ( $y < $global_y ) and ( $y > 0 ) );
+    for ( @{$objects} ) {
+        if ( ( $x == $_->get_x ) and ( $y == $_->get_y ) ) {return 1};
     }
     return 0;
 }
 
 sub print_out {
-    my ($objects, $global_x, $global_y, $matrix) = @_;
-    for my $row (0..$global_x) {
-        for my $elem (0..$global_y) {
+    my ( $objects, $global_x, $global_y ) = @_;
+    my $matrix;
+    for my $row ( 0 .. $global_x ) {
+        for my $elem ( 0 .. $global_y ) {
             $matrix->[$row][$elem] = " ";
         }
     }
-    for (@{$objects}) {
+    for ( @{$objects} ) {
         $matrix->[$_->get_x][$_->get_y] = "x";
     }
     print "\033[2J";
-    for (@{$matrix}) {
-        for (@$_) {
+    for ( @{$matrix} ) {
+        for ( @{$_} ) {
             print "$_";
         }
         print "\n";
@@ -77,10 +58,10 @@ sub print_out {
 
 sub move_all {
     my $objects = shift;
-    for (@{$objects}) {
-        my ($new_x, $new_y) = ($_->move());
-        unless (check($new_x, $new_y, $objects, @_)) {
-            $_->set_x($new_x); 
+    for ( @{$objects} ) {
+        my ( $new_x, $new_y ) = ( $_->move() );
+        unless ( check( $new_x, $new_y, $objects, @_ ) ) {
+            $_->set_x($new_x);
             $_->set_y($new_y);
         }
     }
@@ -89,18 +70,31 @@ sub move_all {
 sub move {
     my $self = shift;
     my $dir = int rand 8;
-    my ($new_x, $new_y);
+    my ( $new_x, $new_y );
     given ($dir) {
-        when(0) {$new_x = $self->{x}+1; $new_y = $self->{y}  ;}
-        when(1) {$new_x = $self->{x}+1; $new_y = $self->{y}+1;}
-        when(2) {$new_x = $self->{x}  ; $new_y = $self->{y}+1;}
-        when(3) {$new_x = $self->{x}-1; $new_y = $self->{y}+1;}
-        when(4) {$new_x = $self->{x}-1; $new_y = $self->{y}  ;}
-        when(5) {$new_x = $self->{x}-1; $new_y = $self->{y}-1;}
-        when(6) {$new_x = $self->{x}  ; $new_y = $self->{y}-1;}
-        when(7) {$new_x = $self->{x}+1; $new_y = $self->{y}-1;}
+        when(0) { $new_x = $self->{x}+1; $new_y = $self->{y}  ; }
+        when(1) { $new_x = $self->{x}+1; $new_y = $self->{y}+1; }
+        when(2) { $new_x = $self->{x}  ; $new_y = $self->{y}+1; }
+        when(3) { $new_x = $self->{x}-1; $new_y = $self->{y}+1; }
+        when(4) { $new_x = $self->{x}-1; $new_y = $self->{y}  ; }
+        when(5) { $new_x = $self->{x}-1; $new_y = $self->{y}-1; }
+        when(6) { $new_x = $self->{x}  ; $new_y = $self->{y}-1; }
+        when(7) { $new_x = $self->{x}+1; $new_y = $self->{y}-1; }
     };
-    return($new_x, $new_y);
+    return( $new_x, $new_y );
+}
+
+our $AUTOLOAD;
+
+sub AUTOLOAD {
+    my ( $self,   $arg )  = @_;
+    my ( $prefix, $attr ) = $AUTOLOAD =~ /.*::(set|get)_(.*)/;
+    if ( $prefix eq 'get' ) {
+        return $self->{$attr};
+    }
+    else {
+        $self->{$attr} = $arg;
+    }
 }
 
 1;
--- lab11.pl
+++ lab11.pl
@@ -4,21 +4,21 @@ use strict;
 use warnings;
 use Modules::Dots;
 
-# Create a table of cells and a couple of entities, which change their position from one cell to another. 
-# The positions are changed every second. During each pass the entity randomly chooses one of eight 
-# neighbour cells to move toward. The entity should not change a cell if choosed cell is occupied by another 
-# instance or if it is placed out of table. You should use OOP paradigm to realize the task. 
+# Create a table of cells and a couple of entities, which change their position from one cell to another.
+# The positions are changed every second. During each pass the entity randomly chooses one of eight
+# neighbour cells to move toward. The entity should not change a cell if choosed cell is occupied by another
+# instance or if it is placed out of table. You should use OOP paradigm to realize the task.
 # The table and instances should be printed to STDOUT.
 
-my @obj;
-my ($global_x, $global_y, $count) = (20, 40, 10);
+my @obj = ();
+my ( $global_x, $global_y, $count ) = ( 20, 40, 10 );
 
-for (1..$count) {
-    push @obj, Modules::Dots->new($global_x, $global_y, \@obj);
+for ( 1 .. $count ) {
+    push @obj, Modules::Dots->new( $global_x, $global_y, \@obj );
 }
 
-for (;;) {
-    Modules::Dots::move_all(\@obj, $global_x, $global_y);
-    Modules::Dots::print_out(\@obj, $global_x, $global_y);
-    select(undef, undef, undef, 0.1);
+for ( ; ; ) {
+    Modules::Dots::move_all( \@obj, $global_x, $global_y );
+    Modules::Dots::print_out( \@obj, $global_x, $global_y );
+    select( undef, undef, undef, 0.1 );
 }
