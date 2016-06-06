9b62d814125696fce2a111e5c7b77a1a66c3e9f2
Oleksandr Halahan <Oleksandr.Halahan@gmail.com>
2016-06-06 07:00:51 +0300

    improve statistics

--- lab13.pl
+++ lab13.pl
@@ -9,36 +9,59 @@ use warnings;
 # SIGUSR2 - print the amount of free memory;
 # SIGINT - send USR1 to itself and quit the program.
 
+### Subroutines
 {
     my $counter = 0;
     sub get_count {
         $counter++;
         if ( $counter == 10 ) {
             $counter = 0;
-            return undef;
+            return 1;
         }
-        return 1;
+        return undef;
     }
 }
 
 sub results {
     my $data = shift;
-    my %own_hash = undef;
-    %own_hash = %{$data};
-}
-
-my %data = undef;
-
-while (1) {
-    if (get_count) {
-        $data{stat} = qx( ps -A | wc -l );
-        $data{memo} = qx( free -m | tail -2 | head -1 | awk {'print $4'} );
+    my %own_data;
+    if ($data) {
+        %own_data = %{$data};
     }
     else {
+        $own_data{memo} = qx( free -m | tail -2 | head -1 | awk {'print \$4'} );
+        return ( \%own_data );
+    }
+    return undef;
+}
+
+sub print_stat {
+    my $data = results;
+    $data->{stat} ? last : $data->{stat} = 0;
+    print "Average statistics for processes: $data->{stat}\n";
+    return undef;
+}
+
+sub print_memo {
+    my $data = results;
+    print "Free memory: $data->{memo}\n";
+    return undef;
+}
+### End of subroutines
+
+my %data;
+while (1) {
+    if ( !get_count ) {
+        $data{stat} += qx( ps -A | wc -l );
+
+    }
+    else {
+        $data{stat} = ( $data{stat} / 10 );
+        print "$data{stat}\n";
         results( \%data );
         $data{stat} = 0;
-        $data{memo} = 0;
     }
-
+    print_memo;
+    print_stat;
     sleep 1;
 }
