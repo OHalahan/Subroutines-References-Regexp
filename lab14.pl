81527ddf7086b1759d6b1c883f7fc721690bda3d
Oleksandr Halahan <Oleksandr.Halahan@gmail.com>
2016-06-08 07:29:46 +0300

    started work on regexp

--- lab14.pl
+++ lab14.pl
@@ -18,3 +18,22 @@ use warnings;
 # ...
 #
 # File order in the results may be arbitrary. Use file locking to prevent a mess in the file.
+
+my $pattern = shift @ARGV;
+my $regexp  = qr/$pattern/;
+my @files   = @ARGV;
+
+for my $file (@files) {
+    
+
+    print "Given regexp: $pattern\n";
+    print "$file:\n";
+    open( my $fh, '<', $file ) or die "Cannot open a file $file: $!\n";
+
+    while ( <$fh> ) {
+        while ( m/(\b\w*$regexp\w*\b)/isg ) {
+            print "\tline $.: $1\n";
+        }
+    }
+    exit 0;
+}
