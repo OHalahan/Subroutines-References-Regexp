6589b643ccb9fc974227b7ed112125657cc53e2f
Oleksandr Halahan <Oleksandr.Halahan@gmail.com>
2016-06-08 10:33:55 +0300

    rewritten script to work with real past 10 seconds

--- lab13.pl
+++ lab13.pl
@@ -10,26 +10,20 @@ use warnings;
 # SIGINT - send USR1 to itself and quit the program.
 
 ### Subroutines
-{
-    my $counter = 0;
-    sub get_count {
-        $counter++;
-        if ( $counter == 10 ) {
-            $counter = 0;
-            return 1;
-        }
-        return undef;
+
+sub average {
+    my @summary = @_;
+    my $sum     = 0;
+    print @summary . "\n";
+    for (@summary) {
+        $sum += $_;
     }
+    return int ( $sum / @summary );
 }
 
 sub print_stat {
     my $own_data = shift;
-    if ( $own_data->{stat} ) {
         print "\nAverage running processes: $own_data->{stat}\n";
-    }
-    else {
-        print "\nNo statistics yet.\nCurrent number of running processes: $own_data->{current}\n";
-    }
     return undef;
 }
 
@@ -46,22 +40,23 @@ sub quit {
 
 ### End of subroutines
 
-my ( $sum, %data ) = ( 0 );
+my ( $sum, @ten_sec ) = ( 0, () );
+my %data;
 
 $SIG{USR1}  = sub { print_stat( \%data ); print_memo( \%data ) };
 $SIG{USR2}  = sub { print_memo( \%data ) };
 $SIG{INT}   = \&quit;
 
 while (1) {
-    if ( !get_count ) {
-        chomp( $data{memo}    = qx( free -m | tail -2 | head -1 | awk {'print \$4'} ) );
-        # -1 to exclude header of ps command
-        chomp( $data{current} = ( qx( ps -A | wc -l ) - 1 ) );
-        $sum += $data{current};
+    # -1 to exclude header of ps command
+    chomp( my $current_value = ( qx( ps -A | wc -l ) - 1 ) );
+    push @ten_sec, $current_value;
+    if ( @ten_sec > 10 ) {
+        shift @ten_sec;
     }
-    else {
-        $data{stat} = ( $sum / 10 );
-        $sum = 0;
-    }
+
+    $data{stat} = average(@ten_sec);
+    chomp( $data{memo} = qx( free -m | tail -2 | head -1 | awk {'print \$4'} ) );
+
     sleep 1;
 }
