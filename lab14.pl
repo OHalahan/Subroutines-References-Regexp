fcec9dc8c81fbd6a537e2d780ce3e55230555766
Oleksandr Halahan <Oleksandr.Halahan@gmail.com>
2016-06-09 07:16:47 +0300

    almost done; improve rexexp passing

--- lab14.pl
+++ lab14.pl
@@ -2,6 +2,7 @@
 
 use strict;
 use warnings;
+use Fcntl qw( :flock );
 
 # Write a program which searches for all matches of a given pattern in a list of files. It should use a separate process for each file, i.e. to process all files simultaneously. The result should be written to a single file ./results.txt. An example:
 #
@@ -19,21 +20,51 @@ use warnings;
 #
 # File order in the results may be arbitrary. Use file locking to prevent a mess in the file.
 
-my $pattern = shift @ARGV;
+my ( $pattern, @files ) = @ARGV;
+my ( $output_file, @kids ) = ( './results.txt', () );
 my $regexp  = qr/$pattern/;
-my @files   = @ARGV;
+
+open( my $ofh, '>>', $output_file ) or die "Cannot create a file $output_file: $!\n";
+print $ofh "\nGiven regexp: $pattern\n";
 
 for my $file (@files) {
-    
+    my $pid = fork;
 
-    print "Given regexp: $pattern\n";
-    print "$file:\n";
-    open( my $fh, '<', $file ) or die "Cannot open a file $file: $!\n";
-
-    while ( <$fh> ) {
-        while ( m/(\b\w*$regexp\w*\b)/isg ) {
-            print "\tline $.: $1\n";
-        }
+    if ($pid) {
+        push @kids, $pid;
     }
-    exit 0;
+    else {
+        my @findings = ();
+        open( my $fh, '<', $file ) or die "Cannot open a file $file: $!\n";
+        #first, find matches.
+        #No one knows how long it will take for this child
+        #so no need to prevent others from writing at this step
+        while ( <$fh> ) {
+            chomp;
+            while ( m/(\b\w*$regexp\w*\b)/isg ) {
+                push @findings, "line $.: $1";
+            }
+        }
+        #print what was found
+        flock( $ofh, LOCK_EX );
+        print $ofh "\n$file:\n";
+        if (@findings) {
+            for my $row (@findings) {
+                print $ofh "\t$row\n";
+            }
+        }
+        else {
+            print $ofh "\tNo matches found\n";
+        }
+
+        close $fh;
+        exit 0;
+    }
 }
+
+close $ofh;
+
+for my $child (@kids) {
+    waitpid $child, 0;
+}
+print "\nAll files were processed.\n";
