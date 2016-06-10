2088b872950295ca004ff138fa00405ccbb9e88d
Oleksandr Halahan <Oleksandr.Halahan@gmail.com>
2016-06-10 07:27:23 +0300

    works OK but not optimal; need to compare with PIDs at the beginning

---
+++ lab15.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+use Fcntl qw( :flock );
+
+my ( $pattern, @files ) = @ARGV;
+my ( $reader, $writer, @kids ) = ( undef, undef, () );
+my $regexp  = qr/$pattern/;
+
+pipe( $reader, $writer );
+print $writer "\nGiven regexp: $pattern\n";
+
+for my $file (@files) {
+    my $pid = fork;
+
+    if ($pid) {
+        push @kids, $pid;
+    }
+    else {
+        close $reader;
+        my @findings = ();
+        open( my $fh, '<', $file ) or die "\n$file:\n\tCannot open a file $file: $!\n";
+
+        while ( <$fh> ) {
+            chomp;
+            while ( m/($regexp)/isg ) {
+                push @findings, "line $.: $1";
+            }
+        }
+        #print what was found and identical markers
+        print $writer "\nSTART $$\n";
+        print $writer "\n$file:\n";
+        if (@findings) {
+            for my $row (@findings) {
+                print $writer "\t$row\n";
+            }
+        }
+        else {
+            print $writer "\tNo matches found\n";
+        }
+        print $writer "\nEND $$\n";
+        close $fh;
+        exit 0;
+    }
+}
+
+close $writer;
+
+my @input = <$reader>;
+close $reader;
+
+for my $child (@kids) {
+    for (@input) {
+        if ( /^START $child\b/ .. /^END $child\b/ ) {
+            if ( /START/ || /END/ ) { next; }
+            print;
+        }
+    }
+}
+
+for my $child (@kids) {
+    waitpid $child, 0;
+}
+print "\nAll files were processed.\n";
