4d89e412af4933735f4b7e2b6e8967799a64a589
Oleksandr Halahan <Oleksandr.Halahan@gmail.com>
2016-06-10 06:15:25 +0300

    finished regexp

--- lab14.pl
+++ lab14.pl
@@ -4,22 +4,6 @@ use strict;
 use warnings;
 use Fcntl qw( :flock );
 
-# Write a program which searches for all matches of a given pattern in a list of files. It should use a separate process for each file, i.e. to process all files simultaneously. The result should be written to a single file ./results.txt. An example:
-#
-# > finder.pl REGEXP file1 file2 file3 file4
-#
-# ./results.txt:
-# Given regexp: REGEXP
-# file1:
-#    line  5: MATCH1, MATCH2
-#    line 19: MATCH
-#    ...
-# file2:
-#    line  9: MATCH1
-# ...
-#
-# File order in the results may be arbitrary. Use file locking to prevent a mess in the file.
-
 my ( $pattern, @files ) = @ARGV;
 my ( $output_file, @kids ) = ( './results.txt', () );
 my $regexp  = qr/$pattern/;
@@ -41,7 +25,7 @@ for my $file (@files) {
         #so no need to prevent others from writing at this step
         while ( <$fh> ) {
             chomp;
-            while ( m/(\b\w*$regexp\w*\b)/isg ) {
+            while ( m/($regexp)/isg ) {
                 push @findings, "line $.: $1";
             }
         }
