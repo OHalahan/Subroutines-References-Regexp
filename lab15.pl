a016e0564851478ad4aaaae0e30bea02c4633a2d
Oleksandr Halahan <Oleksandr.Halahan@gmail.com>
2016-06-11 12:05:19 +0300

    finished

--- lab15.pl
+++ lab15.pl
@@ -5,46 +5,42 @@ use warnings;
 use Fcntl qw( :flock );
 
 my ( $pattern, @files ) = @ARGV;
-my ( $reader, $writer, @kids ) = ( undef, undef, () );
+my ( $reader, $writer, @kids, $pid, $row, %read );
 my $regexp  = qr/$pattern/;
 pipe( $reader, $writer );
+
+my $usage = "Usage: ./lab15.pl REGEXP file1\nExample: ./lab15.pl" . ' "\b\w*op\w*\b" ' . "test\n";
+
+if ( @ARGV < 2 ) { die $usage };
 
 for my $file (@files) {
 
     my $pid = fork;
-    if ($pid) {
-        push @kids, $pid;
-    }
+    if ($pid) { push @kids, $pid }
     else {
         close $reader;
-        my @findings = ();
+        my %findings;
         open( my $fh, '<', $file ) or die "\n$file:\n\tCannot open a file $file: $!\n";
 
         while ( <$fh> ) {
             chomp;
             while ( m/($regexp)/isg ) {
-                push @findings, "line $.: $1";
+                if ( $findings{$.} ) { @{ $findings{$.} }[0] .= ", $1" }
+                else { push @{ $findings{$.} }, "line $.: $1" }
             }
         }
         #print what was found and identical markers
         print $writer "\n$$ $file:\n";
-        if (@findings) {
-            for my $row (@findings) {
-                print $writer "$$ \t$row\n";
-            }
+        if ( keys %findings ) {
+            for my $row ( sort { $a <=> $b } ( keys %findings ) ) { print $writer "$$ \t@{ $findings{$row} }\n" }
         }
-        else {
-            print $writer "$$ \tNo matches found\n";
-        }
+        else { print $writer "$$ \tNo matches found\n" }
         close $fh;
         exit 0;
     }
 }
 
 close $writer;
-
-my ( $pid, $row, %read );
-
 print "\nGiven regexp: $pattern\n";
 
 #form hash of anonymous arrays where key is a PID of a child;
@@ -56,13 +52,9 @@ while (<$reader>) {
 }
 
 for my $child (@kids) {
-    if ( $read{$child} ) {
-        print "@{ $read{$child} }";
-    }
+    if ( $read{$child} ) { print "@{ $read{$child} }" }
+    waitpid $child, 0;
 }
 
 close $reader;
-for my $child (@kids) {
-    waitpid $child, 0;
-}
 print "\nAll files were processed.\n";
